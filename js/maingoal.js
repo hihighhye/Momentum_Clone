4f9561ffacf7bfaf67a5a34b3f6b9fe3e2d8450d
hihighhye <hihye.j.kim@gmail.com>
2025-04-16 16:31:52 +0900

    Moduling without main.js

--- js/maingoal.js
+++ js/maingoal.js
@@ -1,40 +1,42 @@
-export function initMaingoal({
-    goalForm,
-    goalInput,
-    usersGoalContainer,
-    usersGoal,
-    deleteBtn,
-}, HIDDEN_CLASSNAME, GOAL_KEY) {
-    function paintGoal(goal) {
-        usersGoal.innerText = goal;
-        deleteBtn.addEventListener("click", deleteGoal);
-        goalForm.classList.add(HIDDEN_CLASSNAME);
-        usersGoalContainer.classList.remove(HIDDEN_CLASSNAME);
-    }
-    
-    function handleGoalSubmit(event) {
-        event.preventDefault();
-        const goal = goalInput.value;
-        goalInput.value = "";
-        paintGoal(goal);
-    
-        localStorage.setItem(GOAL_KEY, goal);
-    }
-    
-    function deleteGoal(event) {
-        usersGoalContainer.classList.add(HIDDEN_CLASSNAME);
-        goalForm.classList.remove(HIDDEN_CLASSNAME);
-    
-        localStorage.setItem(GOAL_KEY, "");
-    }
-    
-    
-    goalForm.addEventListener("submit", handleGoalSubmit);
-    
-    const savedGoal = localStorage.getItem(GOAL_KEY);
-    
-    if (savedGoal) {
-        paintGoal(savedGoal);
-    }
+import { HIDDEN_CLASSNAME, GOAL_KEY } from "./SecretKeys.js"
+
+const goalForm = document.getElementById("goal-form");
+const goalInput = document.getElementById("main-goal");
+const usersGoalContainer = document.getElementById("users-goal-container");
+const usersGoal = document.querySelector("#users-goal");
+const deleteBtn = document.querySelector("#delete-button");
+
+
+function paintGoal(goal) {
+    usersGoal.innerText = goal;
+    deleteBtn.addEventListener("click", deleteGoal);
+    goalForm.classList.add(HIDDEN_CLASSNAME);
+    usersGoalContainer.classList.remove(HIDDEN_CLASSNAME);
 }
+
+function handleGoalSubmit(event) {
+    event.preventDefault();
+    const goal = goalInput.value;
+    goalInput.value = "";
+    paintGoal(goal);
+
+    localStorage.setItem(GOAL_KEY, goal);
+}
+
+function deleteGoal(event) {
+    usersGoalContainer.classList.add(HIDDEN_CLASSNAME);
+    goalForm.classList.remove(HIDDEN_CLASSNAME);
+
+    localStorage.setItem(GOAL_KEY, "");
+}
+
+
+goalForm.addEventListener("submit", handleGoalSubmit);
+
+const savedGoal = localStorage.getItem(GOAL_KEY);
+
+if (savedGoal) {
+    paintGoal(savedGoal);
+}
+
 
--- js/weather.js
+++ js/weather.js
@@ -1,22 +1,23 @@
 import { OPENWEATHER_API_KEY } from "./SecretKeys.js";
 
-export function initWeather(weatherElement, cityElement) {
-    function onGeoOk(position) {
-        const lat = position.coords.latitude;
-        const lon = position.coords.longitude;
-    
-        const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${OPENWEATHER_API_KEY}&units=metric`;
-        fetch(url).then(response => response.json())
-            .then(data => {
-                cityElement.innerText = data.name;
-                weatherElement.innerText = `${data.weather[0].main} / ${data.main.temp}°C`;
-            });
-    
-    }
-    
-    function onGeoError() {
-        alert("Can't find you. No weather for you.");
-    }
-    
-    navigator.geolocation.getCurrentPosition(onGeoOk, onGeoError);
+
+function onGeoOk(position) {
+    const lat = position.coords.latitude;
+    const lon = position.coords.longitude;
+
+    const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${OPENWEATHER_API_KEY}&units=metric`;
+    fetch(url).then(response => response.json())
+        .then(data => {
+            const weatherElement = document.querySelector("#weather-container span:first-child");
+            const cityElement = document.querySelector("#weather-container span:last-child");
+            cityElement.innerText = data.name;
+            weatherElement.innerText = `${data.weather[0].main} / ${data.main.temp}°C`;
+        });
+
 }
+
+function onGeoError() {
+    alert("Can't find you. No weather for you.");
+}
+
+navigator.geolocation.getCurrentPosition(onGeoOk, onGeoError);
